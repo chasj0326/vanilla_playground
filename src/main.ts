c42c0c24d91bc54d35034684c5fafdb5bb3ed7f1
Sejin Cha <chasj0326@naver.com>
2024-02-20 20:55:31 +0900

    init: 프로젝트 초기화

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,7 @@
-import HomePage from './pages/HomePage';
-import CountPage from './pages/CountPage';
-import createRouter from './routes/Router';
 import './style.css';
 import App from './App';
-import AboutPage from './pages/AboutPage';
-import PostPage from './pages/PostPage';
-import DetailPage from './pages/DetailPage';
 
 const $app = document.querySelector<HTMLElement>('#app');
-new App({ target: $app! });
-
-export const router = createRouter([
-  {
-    path: '/',
-    component: HomePage,
-    children: [
-      {
-        path: '/count',
-        component: CountPage,
-      },
-      {
-        path: '/about',
-        component: AboutPage,
-      },
-      {
-        path: '/post',
-        component: PostPage,
-        children: [
-          {
-            path: '/:id',
-            component: DetailPage,
-          },
-        ],
-      },
-    ],
-  },
-]);
-
-router.init();
+if ($app) {
+  new App({ target: $app });
+}
