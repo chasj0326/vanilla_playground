098164e964b736a8798319a8613dd1eb4a065c31
Sejin Cha <chasj0326@naver.com>
2024-02-28 20:24:50 +0900

    feat: seperator 환경변수 전환

--- src/utils/emoji/parseWithEmoji.ts
+++ src/utils/emoji/parseWithEmoji.ts
@@ -1,7 +1,9 @@
+const seperator = import.meta.env.VITE_EMOJI_SEPERATOR;
+
 export const joinTitleWithEmoji = (emoji: string, title: string) =>
-  [emoji, title].join('@@@');
+  [emoji, title].join(seperator);
 
 export const splitTitleWithEmoji = (title: string) => {
-  const [emojiValue, titleValue] = title.split('@@@');
+  const [emojiValue, titleValue] = title.split(seperator);
   return [emojiValue || '', titleValue || ''] as const;
 };
