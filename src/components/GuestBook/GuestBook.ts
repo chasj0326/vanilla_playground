9454205e335430f0cb5a5d7eb9a0ca3c03236a17
Sejin Cha <chasj0326@naver.com>
2024-03-26 20:02:27 +0900

    feat: 방명록 기능 1차 완성

--- src/components/GuestBook/GuestBook.ts
+++ src/components/GuestBook/GuestBook.ts
@@ -2,7 +2,7 @@ import WriteForm from "./WriteForm";
 import { Component } from "@notion/core";
 import { guestBookData, store } from "@notion/store";
 import { guestBook } from "@notion/services";
-import { GuestBookData } from "@notion/types";
+import { GuestBookData, GuestContent } from "@notion/types";
 import { STORE_KEY } from "@notion/constants";
 
 class GuestBook extends Component {
@@ -13,46 +13,31 @@ class GuestBook extends Component {
     });
   }
 
+  handleSubmit(guestContent: GuestContent, id?: string) {
+    if (!id) guestBook.createGuestBook(guestContent);
+    else guestBook.updateGuestBook(id, guestContent);
+  }
+
   mounted(): void {
     guestBook.getGuestBooks();
 
     this.addEvent("click", (target) => {
-      const listElId = target.closest("li")?.id ?? "";
-      const id = listElId.substring(1);
+      const id = (target.closest("li")?.id ?? "").substring(1);
       const action = target.closest("button")?.dataset.action;
 
-      if (!id) {
-        if (action === "add") {
-          guestBook.createGuestBook({
-            updateAt: "",
-            username: "chacha",
-            content: "어렵구먼",
-            password: "123",
-            profile: {
-              background: "",
-              charactor: "",
-            },
-          });
-        }
-        return;
-      }
-
-      const targetBook = store.getData<GuestBookData>(guestBookData)[id];
+      if (!id) return;
+      const guestContent = store.getData<GuestBookData>(guestBookData)[id];
 
       switch (action) {
-        case "delete": {
-          console.log("delete", id);
-          guestBook.deleteGuestBook(id);
-          break;
-        }
         case "update": {
-          console.log("update", id);
           this.addComponent(WriteForm, {
             selector: `#_${id}`,
             props: {
-              onSubmit: () => {},
-              onCancel: () => {},
-              initial: { ...targetBook, password: "" },
+              onSubmit: (newContent: GuestContent) =>
+                this.handleSubmit(newContent, id),
+              onCancel: () => this.render(),
+              onDelete: () => guestBook.deleteGuestBook(id),
+              initial: { ...guestContent, password: "" },
             },
           });
           break;
@@ -63,23 +48,32 @@ class GuestBook extends Component {
     });
   }
 
+  rendered() {
+    this.addComponent(WriteForm, {
+      selector: ".add-item",
+      props: {
+        onSubmit: (newContent: GuestContent) => this.handleSubmit(newContent),
+        onCancel: () => this.render(),
+      },
+    });
+  }
+
   template(): string {
     const guestBooks = store.getData<GuestBookData>(guestBookData);
     return `
+      <div class="add-item"></div>
       <ul class='guest-book-list'>${Object.entries(guestBooks)
         .map(
           ([id, { username, content }]) => `
           <li id='_${id}' class='guest-book-item'>
             <span>${username}</span>:
             <span>${content}</span>
-            <button data-action="delete">삭제</button>
-            <button data-action="update">수정</button>
+            <button data-action="update">삭제/수정</button>
           </li>
         `,
         )
         .join("")}
       </ul>
-      <button data-action="add">추가</button>
     `;
   }
 }
--- src/components/GuestBook/WriteForm.ts
+++ src/components/GuestBook/WriteForm.ts
@@ -3,24 +3,76 @@ import { GuestContent } from "@notion/types";
 
 interface WriteFormProps {
   initial?: GuestContent;
-  onSubmit: VoidFunction;
-  onCancel: VoidFunction;
+  onSubmit: (guestContent: GuestContent) => void;
+  onCancel?: VoidFunction;
+  onDelete?: VoidFunction;
 }
 
 class WriteForm extends Component<WriteFormProps> {
+  makeForm(): GuestContent {
+    const username =
+      this.findElement<HTMLInputElement>("[name='username']").value;
+    const content =
+      this.findElement<HTMLInputElement>("[name='content']").value;
+    const password =
+      this.findElement<HTMLInputElement>("[name='password']").value;
+    const updateAt = "";
+    const profile = {
+      background: "",
+      charactor: "",
+    };
+
+    return { username, content, password, updateAt, profile };
+  }
+
+  mounted(): void {
+    this.addEvent("click", (target) => {
+      const action = target.closest("button")?.dataset.action;
+      const onSubmit = this.props?.onSubmit;
+      const onCancel = this.props?.onCancel;
+      const onDelete = this.props?.onDelete;
+
+      if (!onSubmit) return;
+
+      switch (action) {
+        case "submit": {
+          onSubmit(this.makeForm());
+          break;
+        }
+        case "cancel": {
+          if (onCancel) onCancel();
+          break;
+        }
+        case "delete": {
+          if (onDelete) onDelete();
+          break;
+        }
+      }
+    });
+  }
   template(): string {
     const {
       username = "",
       content = "",
       password = "",
     } = this.props?.initial ?? {};
+    const formForNew = Boolean(this.props?.initial);
+
     return `
-      <form>
-        <input id="username" value="${username}"/>
-        <input id="content" value="${content}"/>
-        <input id="password" value="${password}"/>
-        <button>완료</button>
-      </form>
+      <div>
+        <input name="username" value="${username}" placeholder="닉네임"/>
+        <input name="content" value="${content}" placeholder="내용"/>
+        <input name="password" value="${password}" placeholder="비밀번호"/>
+        <button type="button" data-action="submit">완료</button>
+        
+        ${
+          formForNew
+            ? `
+            <button type="button" data-action="cancel">취소</button>
+            <button type="button" data-action="delete">삭제</button>`
+            : ""
+        }
+      </div>
     `;
   }
 }
